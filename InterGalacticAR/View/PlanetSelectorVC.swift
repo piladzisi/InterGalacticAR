20cdd07712f9694ed4159d90d1105168de9f73c1
piladzisi <piladzisi@gmail.com>
2019-07-22 22:35:25 +0300

    initial commit

---
+++ InterGalacticAR/View/PlanetSelectorVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlanetSelectorVC.swift
+//  InterGalacticAR
+//
+//  Created by anna.sibirtseva on 22/07/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class PlanetSelectorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
